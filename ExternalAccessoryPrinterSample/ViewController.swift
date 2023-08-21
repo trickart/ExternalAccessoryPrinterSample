531c41bb70fef993c53f4da3f98583fab83f8e37
trickart <ryotaro.seki@ubiregi.com>
2023-08-21 21:54:34 +0900

    Add project

---
+++ ExternalAccessoryPrinterSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExternalAccessoryPrinterSample
+//
+//  Created by trickart on 2023/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
